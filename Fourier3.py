38476199a59d6c0c54f5d9c0fbfdc3674857dcb7
A Santiago Jimenez G <asjimenezg1923@gmail.com>
2022-02-21 23:17:21 -0500

    Cambios

--- Fourier3.py
+++ Fourier3.py
@@ -6,10 +6,10 @@ t = np.linspace(-2, 2, 1000, endpoint=True)
 functions=[]
 harmonics=10
 for i in range(1,harmonics,1):
-    functions.append(np.sin(2*i*np.pi*t)/i)
+    functions.append(((-1**(i+1))/i)*np.sin(2*i*np.pi*t))
 
-plt.plot(t,5+5*scipy.signal.sawtooth(2 * np.pi * 1 * t))
-plt.plot(t,5-(10/np.pi)*np.sum(functions,axis=0))
+plt.plot(t,5*scipy.signal.sawtooth(2 * np.pi * 1 * t))
+plt.plot(t,(10/np.pi)*np.sum(functions,axis=0))
 
 plt.xlabel('Time') 
 plt.ylabel('Amplitude') 
@@ -17,3 +17,5 @@ plt.title('Sawtooth Signal')
 plt.grid()
 plt.autoscale()
 plt.show() 
+
+
