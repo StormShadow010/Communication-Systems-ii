f2b432d1e58aea8541cde3125f22fc57aae290bf
A Santiago Jimenez G <asjimenezg1923@gmail.com>
2022-03-10 15:30:38 -0500

    Cambios

--- Duty_Cycle.py
+++ Duty_Cycle.py
@@ -1,8 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-p_duty_Cycle=np.arange(0.1,0.6,0.1)
-Amplitude=1
+p_duty_Cycle=np.arange(0.4,0.7,0.1)
+Amplitude=10
+Period=10
+Wo=2*np.pi/Period
+# Duty Cycle = p_duty_Cycle[i]/T
 C_Magnitudes=[]
 C_Phases=[]
 
@@ -11,8 +14,9 @@ for i in range(0,len(p_duty_Cycle),1):#Recorrido por los diferentes ciclos de tr
     Phase=[]
     n=[]
     for j in range(-30,30,1): #Evaluación de los coefientes para cada Cn
-        if j==0: Magnitude.append(Amplitude*p_duty_Cycle[i])
-        else: Magnitude.append((Amplitude*p_duty_Cycle[i])*(np.sin(j*np.pi*p_duty_Cycle[i]))/(j*np.pi*p_duty_Cycle[i]))
+        teta=j*Wo*p_duty_Cycle[i]/2
+        if j==0: Magnitude.append((Amplitude*p_duty_Cycle[i])/Period)
+        else: Magnitude.append(((Amplitude*p_duty_Cycle[i])/Period)*(np.sin(teta)/(teta)))
         n.append(j)
     for k in range(0,len(Magnitude),1): #Evaluación para los grados por cada Cn
         phase=0 if Magnitude[k]>0 else 180
@@ -24,6 +28,7 @@ fig, axs = plt.subplots(len(p_duty_Cycle),figsize=(50,400))
 for i in range(0,len(p_duty_Cycle),1):#Gráficación
     axs[i].set_title(f"Duty Cycle {round(p_duty_Cycle[i],1)}")
     axs[i].stem(n,np.abs(C_Magnitudes[i]))
+    
 plt.subplots_adjust(hspace=1)
 plt.show()
 
